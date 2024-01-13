a08dc7516db0e28937b226f4052c56e1d2f6dc45
weiqiushi <weiqiushi@buckyos.com>
2024-01-13 14:46:02 +0800

    Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,68 +2,133 @@ import { ethers, network, upgrades } from "hardhat";
 import * as fs from "node:fs";
 import { Exchange, PublicDataStorage } from "../typechain-types";
 
-async function main() {
-    let depolyedInfo: any = {};
+let depolyedInfo: any = {};
+
+if (fs.existsSync(`${network.name}-deployed.json`)) {
+    depolyedInfo = JSON.parse(fs.readFileSync(`${network.name}-deployed.json`, { encoding: 'utf-8' }));
+}
+
+function saveInfo() {
+    fs.writeFileSync(`${network.name}-deployed.json`, JSON.stringify(depolyedInfo, null, 4));
+}
+
+export async function main() {
     // 部署DMC合约，精度18，最大值10亿
     // 基金会账户初始分配15W
-    // TODO：基金会账户地址？
-    let foundationAddress = "";
+    let foundationAddress = "0x846cc73d43bda4bcd57cb23678b31f2bf24f801a";
     // for test:
+    let depolyAddr = (await ethers.getSigners())[0].address;
     if (network.name === "localhost") {
         foundationAddress = (await ethers.getSigners())[19].address;
+        console.log("depoly address:", depolyAddr);
     }
-    console.log("Deploying DMC...");
-    const dmcContract = await (await ethers.deployContract("DMCToken", [ethers.parseEther("1000000000"), [foundationAddress], [ethers.parseEther("150000")]])).waitForDeployment();
-    depolyedInfo.DMCToken = await dmcContract.getAddress();
-    // 部署GWT合约
-    console.log("Deploying GWT...");
-    const gwtContract = await (await ethers.deployContract("GWTToken")).waitForDeployment();
-    depolyedInfo.GWTToken = await gwtContract.getAddress();
 
-    console.log("Deploying LuckyMint...");
-    const luckyMint = await (await ethers.deployContract("LuckyMint")).waitForDeployment();
-    depolyedInfo.LuckyMint = await luckyMint.getAddress();
+    let dmcContract;
 
-    // 部署兑换合约
-    console.log("Deploying Exchange...");
-    let exchange = await (await upgrades.deployProxy(await ethers.getContractFactory("Exchange"),
-        [depolyedInfo.DMCToken, depolyedInfo.GWTToken],
-        {
-            initializer: "initialize",
-            kind: "uups",
-            timeout: 0
-        })).waitForDeployment() as unknown as Exchange;
-    depolyedInfo.exchange = await exchange.getAddress();
+    if (!depolyedInfo.DMCToken) {
+        console.log("Deploying DMC...");
+        dmcContract = await (await ethers.deployContract("DMCToken", [ethers.parseEther("1000000000"), [], []])).waitForDeployment();
+        depolyedInfo.DMCToken = await dmcContract.getAddress();
+        saveInfo();
+    } else {
+        console.log("DMC already deployed, address", depolyedInfo.DMCToken);
+        dmcContract = await ethers.getContractAt("DMCToken", depolyedInfo.DMCToken);
+    }
 
-    console.log("set minter...");
-    await (await dmcContract.enableMinter([await exchange.getAddress()])).wait();
-    await (await gwtContract.enableMinter([await exchange.getAddress()])).wait();
-    // TODO: 处理allowMintDMC的权限问题
+    let gwtContract;
+    
+    if (!depolyedInfo.GWTToken) {
+        // 部署GWT合约
+        console.log("Deploying GWT...");
+        gwtContract = await (await ethers.deployContract("GWTToken")).waitForDeployment();
+        depolyedInfo.GWTToken = await gwtContract.getAddress();
+        saveInfo();
+    } else {
+        console.log("GWT already deployed, address", depolyedInfo.GWTToken);
+        gwtContract = await ethers.getContractAt("GWTToken", depolyedInfo.GWTToken);
+    }
+
+    let luckyMint;
+    
+    if (!depolyedInfo.LuckyMint) {
+        console.log("Deploying LuckyMint...");
+        luckyMint = await (await ethers.deployContract("LuckyMint")).waitForDeployment();
+        depolyedInfo.LuckyMint = await luckyMint.getAddress();
+        saveInfo();
+    } else {
+        console.log("LuckyMint already deployed, address", depolyedInfo.LuckyMint);
+        luckyMint = await ethers.getContractAt("LuckyMint", depolyedInfo.LuckyMint);
+    }
+
+    let exchange;
+    
+    if (!depolyedInfo.exchange) {
+        // 部署兑换合约
+        console.log("Deploying Exchange...");
+        exchange = await (await upgrades.deployProxy(await ethers.getContractFactory("Exchange"),
+            [depolyedInfo.DMCToken, depolyedInfo.GWTToken],
+            {
+                initializer: "initialize",
+                kind: "uups",
+                timeout: 0
+            })).waitForDeployment() as unknown as Exchange;
+        depolyedInfo.exchange = await exchange.getAddress();
+        saveInfo();
+    } else {
+        console.log("Exchange already deployed, address", depolyedInfo.exchange);
+        exchange = await ethers.getContractAt("Exchange", depolyedInfo.exchange);
+    }
+
+    console.log("set token minter...");
+    await (await dmcContract.enableMinter([depolyedInfo.exchange])).wait();
+    await (await gwtContract.enableMinter([depolyedInfo.exchange])).wait();
 
     // 部署公共数据合约的库合约
-    console.log("Depoly Library...");
-    let listLibrary = await (await ethers.getContractFactory("SortedScoreList")).deploy();
-    let proofLibrary = await (await ethers.getContractFactory("PublicDataProof")).deploy();
+    if (!depolyedInfo.listLibrary) {
+        console.log("Depoly SortedList Library...");
+        let listLibrary = await (await ethers.getContractFactory("SortedScoreList")).deploy();
+        depolyedInfo.listLibrary = await listLibrary.getAddress();
+        saveInfo();
+    } else {
+        console.log("SortedList Library already deployed, address", depolyedInfo.listLibrary);
+    }
 
-    console.log("Depoly PublicDataStorage...");
-    //let foundationAddress = (await ethers.getSigners())[19].address;
-    const publicDataStorage = await (await upgrades.deployProxy(await ethers.getContractFactory("PublicDataStorage", {
-        libraries: {
-            "SortedScoreList": await listLibrary.getAddress(),
-            "PublicDataProof": await proofLibrary.getAddress()
-        }
-    }),
-        [depolyedInfo.GWTToken, foundationAddress],
-        {
-            initializer: "initialize",
-            kind: "uups",
-            timeout: 0,
-            unsafeAllow: ["external-library-linking"],
-        })).waitForDeployment();
-    depolyedInfo.PublicDataStore = await publicDataStorage.getAddress();
+    if (!depolyedInfo.proofLibrary) {
+        console.log("Depoly PublicDataProof Library...");
+        let proofLibrary = await (await ethers.getContractFactory("PublicDataProof")).deploy();
+        depolyedInfo.proofLibrary = await proofLibrary.getAddress();
+        saveInfo();
+    } else {
+        console.log("PublicDataProof Library already deployed, address", depolyedInfo.proofLibrary);
+    }
+
+    let publicDataStorage;
+
+    if (!depolyedInfo.PublicDataStore) {
+        console.log("Depoly PublicDataStorage...");
+        publicDataStorage = await (await upgrades.deployProxy(await ethers.getContractFactory("PublicDataStorage", {
+            libraries: {
+                "SortedScoreList": depolyedInfo.listLibrary,
+                "PublicDataProof": depolyedInfo.proofLibrary
+            }
+        }),
+            [depolyedInfo.GWTToken, foundationAddress],
+            {
+                initializer: "initialize",
+                kind: "uups",
+                timeout: 0,
+                unsafeAllow: ["external-library-linking"],
+            })).waitForDeployment();
+        depolyedInfo.PublicDataStore = await publicDataStorage.getAddress();
+        saveInfo();
+    } else {
+        console.log("PublicDataStorage already deployed, address", depolyedInfo.PublicDataStore);
+        publicDataStorage = await ethers.getContractAt("PublicDataStorage", depolyedInfo.PublicDataStore);
+    }
 
     console.log("GWT enable transfer to publicDataStorage...");
     await (await gwtContract.enableTransfer([depolyedInfo.PublicDataStore])).wait();
+
     // for test
     if (network.name == "localhost") {
         await (await exchange.allowMintDMC([(await ethers.getSigners())[0].address], ["dmc_for_test"], [ethers.parseEther("100000000")])).wait();
@@ -91,16 +156,14 @@ async function main() {
         let bridge = await (await ethers.getContractFactory("OwnedNFTBridge")).deploy();
         await (await publicDataStorage.allowPublicDataContract(await bridge.getAddress())).wait()
         depolyedInfo.Bridge = await bridge.getAddress();
-    }
-
-    if (network.name !== "hardhat") {
-        fs.writeFileSync(`${network.name}-deployed.json`, JSON.stringify(depolyedInfo, null, 4));
+        saveInfo();
     }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
+/*
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
-});
+});*/
