890d2a234ba8aa07b06364f6bab2562b40f2f9cf
liyaxing <liyaxing@buckyos.com>
2024-04-25 17:02:58 +0800

    Fix dividend contract some bugs

--- contracts/dividend.sol
+++ contracts/dividend.sol
@@ -44,7 +44,6 @@ contract DividendContract is ReentrancyGuard {
     struct StakeRecord {
         uint256 cycleIndex;
         uint256 amount;
-        uint256 newAmount;
     }
      mapping(address => StakeRecord[]) UserStakeRecords;
 
@@ -67,12 +66,7 @@ contract DividendContract is ReentrancyGuard {
         return currentCycleIndex;
     }
 
-    function getCurrentCycle() internal returns (CycleInfo storage) {
-        /*
-        uint256 currentCycleIndex = getCurrentCycleIndex();
-        if (cycles.length <= currentCycleIndex) {
-            cycles.push(CycleInfo(block.number, 0, new RewardInfo[](0)));
-        }*/
+    function getCurrentCycle() public view returns (CycleInfo memory) {
         return cycles[currentCycleIndex];
     }
 
@@ -80,7 +74,7 @@ contract DividendContract is ReentrancyGuard {
     function _depositToken(address token, uint256 amount) internal {
         require(amount > 0, "Cannot deposit 0");
 
-        RewardInfo[] storage rewards = getCurrentCycle().rewards;
+        RewardInfo[] storage rewards = cycles[currentCycleIndex].rewards;
         for (uint256 i = 0; i < rewards.length; i++) {
             if (rewards[i].token == token) {
                 rewards[i].amount += amount;
@@ -95,16 +89,16 @@ contract DividendContract is ReentrancyGuard {
 
     
     receive() external payable {
-        _tryNewCycle();
+        tryNewCycle();
 
         _depositToken(address(0), msg.value);
     }
 
     function deposit(uint256 amount, address token) external nonReentrant {
-        _tryNewCycle();
+        tryNewCycle();
 
         require(token != address(stakingToken), "Cannot deposit Staking token");
-        require(token != address(0), "Use native transfer to deposit ETH");
+        require(token != address(0), "Use native transfer to deposit default token");
 
         IERC20(token).transferFrom(msg.sender, address(this), amount);
 
@@ -113,6 +107,13 @@ contract DividendContract is ReentrancyGuard {
 
     function getStakedAmount(address user, uint256 cycleIndex) public view returns (uint256) {
         StakeRecord[] memory stakeRecords = UserStakeRecords[user];
+
+        // print the stake records
+        console.log("will print stake records for user %s", user);
+        for (uint i = 0; i < stakeRecords.length; i++) {
+            console.log("StakeRecords: cycleIndex %d, stake mount %d", stakeRecords[i].cycleIndex, stakeRecords[i].amount);
+        }
+
         for (uint i = stakeRecords.length - 1; ; i--) {
 
             // stakeRecords里面的对应周期的质押数据，都是对应周期发起的操作导致的状态，所以需要进入下一个周期才会生效，所以这里使用 < 而不是 <=
@@ -133,18 +134,17 @@ contract DividendContract is ReentrancyGuard {
         require(amount > 0, "Cannot stake 0");
         require(IERC20(stakingToken).transferFrom(msg.sender, address(this), amount), "Stake failed");
 
-        uint256 currentCycleIndex = getCurrentCycleIndex();
+        console.log("user stake ===> amount %d, cycle %d, user %s", amount, currentCycleIndex, msg.sender);
 
         StakeRecord[] storage stakeRecords = UserStakeRecords[msg.sender];
         if (stakeRecords.length == 0) {
-            stakeRecords.push(StakeRecord(currentCycleIndex, amount, amount));
+            stakeRecords.push(StakeRecord(currentCycleIndex, amount));
         } else {
             StakeRecord storage lastStakeRecord = stakeRecords[stakeRecords.length - 1];
             if (lastStakeRecord.cycleIndex == currentCycleIndex) {
                 lastStakeRecord.amount += amount;
-                lastStakeRecord.newAmount += amount;
             } else {
-                stakeRecords.push(StakeRecord(currentCycleIndex, lastStakeRecord.amount + amount, amount));
+                stakeRecords.push(StakeRecord(currentCycleIndex, lastStakeRecord.amount + amount));
             }
         }
 
@@ -161,50 +161,69 @@ contract DividendContract is ReentrancyGuard {
         StakeRecord[] storage stakeRecords = UserStakeRecords[msg.sender];
         require(stakeRecords.length > 0, "No stake record found");
         
+        console.log("user unstake <=== amount %d, cycle %d, user %s", amount, currentCycleIndex, msg.sender);
+
         // get the last stake record of the user
         StakeRecord storage lastStakeRecord = stakeRecords[stakeRecords.length - 1];
         require(lastStakeRecord.amount >= amount, "Insufficient staked amount");
 
         // 如果存在当前周期的质押操作，那么这个质押操作是可以直接撤销的不影响周期数据(当前质押要在下个周期进入cycleInfo中)
         // 如果不是当前周期的质押操作，或者当前周期的质押数量不足，那么这个质押操作是需要从上个周期关联的cycleInfo数据中减去的
-        uint256 currentCycleIndex = getCurrentCycleIndex();
+        console.log("unstaking amount %d", amount);
+        console.log("currentCycleIndex %d, lastStakeRecord.cycleIndex %d, amount %d", currentCycleIndex, lastStakeRecord.cycleIndex, lastStakeRecord.amount);
+        console.log("lastStakeRecord.cycleIndex %d", lastStakeRecord.cycleIndex);
         if (lastStakeRecord.cycleIndex == currentCycleIndex) {
-            if (lastStakeRecord.newAmount >= amount) {
-                lastStakeRecord.amount -= amount;
-                lastStakeRecord.newAmount -= amount;
-            } else {
-                uint256 diff = amount - lastStakeRecord.newAmount;
 
+            uint256 newAmount = 0;
+            if (stakeRecords.length > 1) {
                 StakeRecord memory prevStakeRecord = stakeRecords[stakeRecords.length - 2];
+                if (prevStakeRecord.amount < lastStakeRecord.amount) {
+                    newAmount = lastStakeRecord.amount - prevStakeRecord.amount;
+                }
+            } else {
+                newAmount = lastStakeRecord.amount;
+            }
+
+            if (newAmount >= amount) {
+                lastStakeRecord.amount -= amount;
+            } else {
+                uint256 diff = amount - newAmount;
+
+                StakeRecord storage prevStakeRecord = stakeRecords[stakeRecords.length - 2];
+                console.log("prevStakeRecord.amount %d", prevStakeRecord.amount);
+                console.log("prevStakeRecord.cycleIndex %d", prevStakeRecord.cycleIndex);
+                console.log("prevStakeRecord.totalStaked %d", cycles[prevStakeRecord.cycleIndex].totalStaked);
                 
                 // the last record is unstaked all and is empty, delete it
-                delete stakeRecords[stakeRecords.length - 1];
+                stakeRecords.pop();
 
                 // the prev record all unstaked with the diff amount
                 prevStakeRecord.amount -= diff;
 
-                // update the total staked amount of the corresponding cycle total staked amount
-                cycles[prevStakeRecord.cycleIndex].totalStaked -= diff;
+                // unstake only effect the current cycle's total staked amount
+                cycles[currentCycleIndex].totalStaked -= diff;
             }
         } else {
             lastStakeRecord.amount -= amount;
 
-            cycles[lastStakeRecord.cycleIndex].totalStaked -= amount;
+            cycles[lastStakeRecord.cycleIndex + 1].totalStaked -= amount;
         }
         
+        totalStaked -= amount;
+
         require(IERC20(stakingToken).transfer(msg.sender, amount), "Unstake failed");
 
         emit Unstake(msg.sender, amount);
     }
 
     // check point for the new cycle
-    function _tryNewCycle() internal {
+    function tryNewCycle() public {
         uint256 currentBlock = block.number;
         
-        CycleInfo storage currentCycle = getCurrentCycle();
+        CycleInfo storage currentCycle = cycles[currentCycleIndex];
         if (currentBlock - currentCycle.startBlock >= cycleMaxLength) {
             currentCycleIndex = currentCycleIndex + 1;
-            console.log("enter new cycle %d", currentCycleIndex);
+            console.log("enter new cycle %d, totalStaked %d", currentCycleIndex, totalStaked);
             CycleInfo storage newCycle = cycles[currentCycleIndex];
             newCycle.startBlock = currentBlock;
             newCycle.totalStaked = totalStaked;
@@ -224,7 +243,7 @@ contract DividendContract is ReentrancyGuard {
     }
 
     // claim rewards for the cycle
-    function withdrawDevidends(uint256[] calldata cycleIndexs, address[] calldata tokens) external nonReentrant {
+    function withdrawDividends(uint256[] calldata cycleIndexs, address[] calldata tokens) external nonReentrant {
         require(cycleIndexs.length > 0, "No cycle index");
         require(tokens.length > 0, "No token");
 
@@ -242,18 +261,19 @@ contract DividendContract is ReentrancyGuard {
 
                 CycleInfo storage cycle = cycles[cycleIndex];
 
-                uint256 totalStaked = cycle.totalStaked;
-                if (totalStaked == 0) {
+                if (cycle.totalStaked == 0) {
                     continue;
                 }
 
                 uint256 userStaked = getStakedAmount(msg.sender, cycleIndex);
+                console.log("userStaked %d, cycle %d", userStaked, cycleIndex);
 
                 // find the token reward of the cycle
                 uint256 rewardAmount = 0;
                 for (uint k = 0; k < cycle.rewards.length; k++) {
                     RewardInfo storage reward = cycle.rewards[k];
                     if (reward.token == token) {
+                        console.log("reward.amount %d, userStaked %d, cycle.totalStaked %d", reward.amount, userStaked, cycle.totalStaked);
                         rewardAmount = reward.amount * userStaked / cycle.totalStaked;
                         break;
                     }
