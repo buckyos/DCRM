be69d98ad756cb2b99e7bd58f0d0540f2282dcae
weiqiushi <weiqiushi@buckyos.com>
2024-04-27 15:21:01 +0800

    Update exchange logic and test case

--- contracts/exchange2.sol
+++ contracts/exchange2.sol
@@ -6,102 +6,138 @@ import "./gwt2.sol";
 import "./dividend.sol";
 
 contract Exchange2 {
-    DMC2 dmcToken;
-    GWTToken2 gwtToken;
-    DividendContract fundationIncome;
+    address dmcToken;
+    address gwtToken;
+    address fundationIncome;
 
     // 当前周期，也可以看做周期总数
-    uint256 current_circle = 1;
+    uint256 current_circle = 0;
     uint256 current_mine_circle_start;
 
     // 这个周期内还能mint多少DMC
     uint256 remain_dmc_balance = 0;
+
+    // 这个周期的能mint的DMC总量
     uint256 current_circle_dmc_balance = 0;
     uint256 current_finish_time = 0;
-    uint256 dmc2gwt_rate = 210;
+    uint256 public dmc2gwt_rate = 210;
+
+    // 总共未mint的DMC总量，这个值会慢慢释放掉
     uint256 total_addtion_dmc_balance = 0;
 
     // 没有挖完的周期总数
     uint256 addtion_circle_count = 0;
     
+    // 周期的最小时长
     uint256 min_circle_time = 100;
 
-    constructor(address _dmcToken, address _gwtToken, address _fundationIncome) {
-        dmcToken = DMC2(_dmcToken);
-        gwtToken = GWTToken2(_gwtToken);
-        fundationIncome = DividendContract(_fundationIncome);
+    uint256 total_mine_period = 420;
+    uint256 adjust_period = 21;
+
+
+    constructor(address _dmcToken, address _gwtToken, address _fundationIncome, uint256 _min_circle_time) {
+        dmcToken = _dmcToken;
+        gwtToken = _gwtToken;
+        fundationIncome = _fundationIncome;
+        min_circle_time = _min_circle_time;
+
+        _newCycle();
     }
 
     function getCircleBalance(uint256 circle_id) public view returns (uint256) {
-        return 210000;
+        uint256 totalDMC = DMC2(dmcToken).totalSupply();
+        return 210 ether;
+    }
+
+    function _newCycle() internal {
+        //移动到下一个周期
+        current_circle = current_circle + 1;
+        current_mine_circle_start = block.timestamp;
+
+        remain_dmc_balance = getCircleBalance(current_circle);
+
+        if(total_addtion_dmc_balance > 0) {
+            uint256 this_addtion_dmc = total_addtion_dmc_balance / addtion_circle_count;
+            total_addtion_dmc_balance -= this_addtion_dmc;
+            
+            remain_dmc_balance += this_addtion_dmc;
+        }
+        
+        current_circle_dmc_balance = remain_dmc_balance;
+        console.log("new cycle %d start at %d, dmc balance %d", current_circle, current_mine_circle_start, current_circle_dmc_balance);
     }
 
     function adjustExchangeRate() internal {
-        if(block.timestamp > current_mine_circle_start + min_circle_time) {
+        if(block.timestamp >= current_mine_circle_start + min_circle_time) {
             //结束当前挖矿周期
+
             if(remain_dmc_balance > 0) {
                 total_addtion_dmc_balance += remain_dmc_balance;
                 addtion_circle_count += 1;
+
+                console.log("prev cycle dmc balance left %d, total left %d, total left cycle %d", remain_dmc_balance, total_addtion_dmc_balance, addtion_circle_count);
+
                 //本周期未挖完，降低dmc2gwt_rate
                 dmc2gwt_rate = dmc2gwt_rate * (1-remain_dmc_balance/current_circle_dmc_balance);
+                if(dmc2gwt_rate < 210) {
+                    // 最低值为210
+                    dmc2gwt_rate = 210;
+                }
+
+                console.log("decrease dmc2gwt_rate to %d", dmc2gwt_rate);
             } else {
+                if (addtion_circle_count > 0) {
+                    addtion_circle_count -= 1;
+                }
                 //本周期挖完了，提高dmc2gwt_rate
                 dmc2gwt_rate = dmc2gwt_rate * (1+(current_finish_time-current_mine_circle_start)/min_circle_time);
+                if(dmc2gwt_rate > 210) {
+                    // 为了测试，最高值也为210
+                    dmc2gwt_rate = 210;
+                }
+                console.log("increase dmc2gwt_rate to %d", dmc2gwt_rate);
             }
-            
-            //移动到下一个周期
-            current_circle = current_circle + 1;
-            current_mine_circle_start = block.timestamp;
 
-            // getCircleBalance(current_circle)的含义是？
-            // 这个circle是刚开始的，这个值是不是一定是0？
-            remain_dmc_balance = getCircleBalance(current_circle);
-
-            if(total_addtion_dmc_balance > 0) {
-                // 此处的addtion_circle_count一定为1，Bug?
-                // total_dmc_balance是什么？
-                uint256 this_addtion_dmc = total_dmc_balance / addtion_circle_count;
-                addtion_circle_count -= 1;
-                // 如何保证total_addtion_dmc_balance一定大于this_addtion_dmc？
-                total_addtion_dmc_balance -= this_addtion_dmc;
-                
-                remain_dmc_balance += this_addtion_dmc;
-            }
-            
-            current_circle_dmc_balance = remain_dmc_balance;
+            _newCycle();
         } else {
+            console.log("keep cycle.");
             require(remain_dmc_balance > 0, "no dmc balance in current circle");
         }
     }
 
-    function exchangeGWT(uint256 amount) public {
-        dmcToken.burnFrom(msg.sender, amount);
-        gwtToken.mint(msg.sender, amount * dmc2gwt_rate);
+    function DMCtoGWT(uint256 amount) public {
+        DMC2(dmcToken).burnFrom(msg.sender, amount);
+        GWTToken2(gwtToken).mint(msg.sender, amount * dmc2gwt_rate * 6 / 5);
     }
 
-    function exchangeDMC(uint256 amount) public {
+    function GWTtoDMC(uint256 amount) public {
         adjustExchangeRate();
         uint256 real_dmc_count = 0;
         uint256 dmc_count = amount / dmc2gwt_rate;
-        if(dmc_count > remain_dmc_balance) {
+        console.log("exchange dmc %d from amount %d, rate %d", dmc_count, amount, dmc2gwt_rate);
+        if(dmc_count >= remain_dmc_balance) {
             current_finish_time = block.timestamp;
             
             real_dmc_count = remain_dmc_balance;
             remain_dmc_balance = 0;
             //不用立刻转给分红合约，而是等积累一下
-            gwtToken.transferFrom(msg.sender, this, real_dmc_count * dmc2gwt_rate);
-            dmcToken.mint(msg.sender, real_dmc_count);
+            GWTToken2(gwtToken).transferFrom(msg.sender, address(this), real_dmc_count * dmc2gwt_rate);
+            DMC2(dmcToken).mint(msg.sender, real_dmc_count);
         } else {
             remain_dmc_balance -= dmc_count;
             //不用立刻转给分红合约，而是等积累一下
-            gwtToken.transferFrom(msg.sender, this, amount);
-            dmcToken.mint(msg.sender, dmc_count);
+            GWTToken2(gwtToken).transferFrom(msg.sender, address(this), amount);
+            DMC2(dmcToken).mint(msg.sender, dmc_count);
         }
     }
     
     // 手工将累积的收入打给分红合约
     function transferIncome() public {
-        gwtToken.approve(fundationIncome, gwtToken.balanceOf(this));
-        fundationIncome.deposit(gwtToken.balanceOf(this));
+        GWTToken2(gwtToken).approve(fundationIncome, GWTToken2(gwtToken).balanceOf(address(this)));
+        DividendContract(payable(fundationIncome)).deposit(GWTToken2(gwtToken).balanceOf(address(this)), address(gwtToken));
+    }
+
+    function getCycleInfo() public view returns (uint256, uint256, uint256) {
+        return (current_circle, remain_dmc_balance, current_circle_dmc_balance);
     }
 }
--- test/test_exchange.ts
+++ test/test_exchange.ts
@@ -1,19 +1,25 @@
 import { ethers, upgrades } from "hardhat"
-import { DMCToken, Exchange, GWTToken } from "../typechain-types"
+import { DMC2, Exchange2, GWTToken2 } from "../typechain-types"
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 import { expect } from "chai";
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("Exchange", function () {
-    let dmc: DMCToken
-    let gwt: GWTToken
-    let exchange: Exchange
+    let dmc: DMC2
+    let gwt: GWTToken2
+    let exchange: Exchange2
     let signers: HardhatEthersSigner[];
 
     before(async () => {
         signers = await ethers.getSigners()
 
-        dmc = await (await ethers.deployContract("DMCToken", [ethers.parseEther("1000000000"), [], []])).waitForDeployment()
-        gwt = await (await ethers.deployContract("GWTToken")).waitForDeployment()
+        console.log("signers 0:", signers[0].address)
+
+        dmc = await (await ethers.deployContract("DMC2", [ethers.parseEther("1000000000"), [signers[0].address], [ethers.parseEther("1000")]])).waitForDeployment()
+        gwt = await (await ethers.deployContract("GWTToken2")).waitForDeployment()
+        exchange = await(await ethers.deployContract("Exchange2", [await dmc.getAddress(), await gwt.getAddress(), ethers.ZeroAddress, 1000])).waitForDeployment()
+        console.log("exchange:", await exchange.getAddress())
+        /*
         exchange = await (await upgrades.deployProxy(await ethers.getContractFactory("Exchange"), 
             [await dmc.getAddress(), await gwt.getAddress()], 
             {
@@ -21,77 +27,87 @@ describe("Exchange", function () {
                 kind: "uups",
                 timeout: 0
             })).waitForDeployment() as unknown as Exchange;
-
-        await (await gwt.enableMinter([await exchange.getAddress()])).wait();
+        */
+        await (await gwt.enableMinter([await exchange.getAddress(), signers[0].address])).wait();
         await (await dmc.enableMinter([await exchange.getAddress()])).wait();
+
+        // 给一些测试用的gwt先
+        await (await gwt.mint(signers[0].address, ethers.parseEther("10000000"))).wait();
     })
 
-    it("mint dmc", async () => {
-        await (await exchange.allowMintDMC([signers[0].address], ["cookie1"], [ethers.parseEther("500")])).wait();
+    it("cycle 1", async () => {
+        expect(await dmc.totalSupply()).to.equal(ethers.parseEther("1000000000"));
+        expect(await dmc.balanceOf(await dmc.getAddress())).to.equal(ethers.parseEther("999999000"));
+        // 兑换DMC到GWT
+        await (await dmc.approve(await exchange.getAddress(), ethers.parseEther("1"))).wait();
 
-        await expect(exchange.connect(signers[1]).allowMintDMC([signers[1].address], ["cookie2"], [ethers.parseEther("1500")])).revertedWith("not mint admin");
-        await expect(exchange.connect(signers[1]).mintDMC("cookie1")).revertedWith("cannot mint");
-
-        await expect(exchange.mintDMC("cookie1")).changeTokenBalance(dmc, signers[0].address, ethers.parseEther("500"));
-
-        await expect(exchange.mintDMC("cookie1")).revertedWith("cannot mint");
-
-        await (await exchange.setMintAdmin(signers[1].address)).wait();
-        await (await exchange.connect(signers[1]).allowMintDMC([signers[1].address], ["cookie2"], [ethers.parseEther("1000")])).wait();
-        await expect(exchange.connect(signers[1]).mintDMC("cookie2")).changeTokenBalance(dmc, signers[1].address, ethers.parseEther("1000"));
+        // 用1 DMC兑换GWT，能兑换1*210*1.2个
+        await expect(exchange.DMCtoGWT(ethers.parseEther("1"))).to.changeTokenBalance(gwt, signers[0], ethers.parseEther((1*210*1.2).toString()));
     });
 
-    it("exchange dmc to gwt", async () => {
-        expect(await dmc.balanceOf(signers[1].address)).to.equal(ethers.parseEther("1000"))
-        let exchangeAddr = await exchange.getAddress();
+    //1476190476190476190
+    //1476190476190476300
 
-        await expect(dmc.connect(signers[1]).approve(exchangeAddr, ethers.parseEther("1")))
-            .emit(dmc, "Approval").withArgs(signers[1].address, exchangeAddr, ethers.parseEther("1"))
+    it("cycle 2", async () => {
+        // 前进到时间1000之后，开启下一轮
+        mine(2, {interval: 1000});
 
-        await expect(exchange.connect(signers[1]).exchangeGWT(ethers.parseEther("1")))
-            .emit(gwt, "Transfer").withArgs(ethers.ZeroAddress, signers[1].address, ethers.parseEther("210"))
-        
-        expect(await gwt.balanceOf(signers[1].address)).to.equal(ethers.parseEther("210"))
-        expect(await dmc.balanceOf(signers[1].address)).to.equal(ethers.parseEther("999"))
-        expect(await dmc.balanceOf(exchangeAddr)).to.equal(ethers.parseEther("1"))
+        // 由于上一轮没有人兑换DMC，本轮的可兑换余额为210 + 210/1=420, rate变为210
+        // 我们兑换320个DMC出来，需要320*210=67200个GWT
+        await(await gwt.approve(await exchange.getAddress(), ethers.parseEther("67200"))).wait();
+        const tx = await exchange.GWTtoDMC(ethers.parseEther("67200"));
+        await expect(tx).to.changeTokenBalance(gwt, signers[0], ethers.parseEther("-67200"));
+        await expect(tx).to.changeTokenBalance(dmc, signers[0], ethers.parseEther("320"));
+        await expect(tx).to.changeTokenBalance(gwt, await exchange.getAddress(), ethers.parseEther("67200"));
+
+        expect((await exchange.getCycleInfo())).to.deep.equal([2n, ethers.parseEther("100"), ethers.parseEther("420")])
     })
 
-    it("unregistered transfer will be reverted", async () => {
-        await expect(gwt.connect(signers[1]).transfer(signers[2].address, ethers.parseEther("1")))
-            .to.be.revertedWith("transfer not allowed")
+    it("cycle 3", async () => {
+        // 前进到时间1000之后，开启下一轮
+        mine(2, {interval: 1000});
+
+        // 上一轮剩余100，此轮的可兑换额度为210 + 100/2 = 260, 储存的可兑换额度为100/2=50
+        // 再兑换160个DMC，需要160*210=33600个GWT
+        await(await gwt.approve(await exchange.getAddress(), ethers.parseEther("33600"))).wait();
+        const tx = await exchange.GWTtoDMC(ethers.parseEther("33600"));
+
+        await expect(tx).to.changeTokenBalance(gwt, signers[0], ethers.parseEther("-33600"));
+        await expect(tx).to.changeTokenBalance(dmc, signers[0], ethers.parseEther("160"));
+        await expect(tx).to.changeTokenBalance(gwt, await exchange.getAddress(), ethers.parseEther("33600"));
+
+        expect((await exchange.getCycleInfo())).to.deep.equal([3n, ethers.parseEther("100"), ethers.parseEther("260")])
     })
 
-    it("register transfer", async () => {
-        // register signers[2]
-        await expect(gwt.connect(signers[0]).enableTransfer([signers[2].address])).to.be.ok;
+    it("cycle 4", async () => {
+        // 前进到时间1000之后，开启下一轮
+        mine(2, {interval: 1000});
 
-        // transfer to signers[2] success
-        await expect(gwt.connect(signers[1]).transfer(signers[2].address, ethers.parseEther("1")))
-            .emit(gwt, "Transfer").withArgs(signers[1].address, signers[2].address, ethers.parseEther("1"))
+        // 上一轮又剩余100，此轮的可兑换额度为210+(100+50)/3=260，储存的可兑换额度为100
+        // 这次我们把260个都兑换掉, 需要260*210=54600个GWT
 
-        expect(await gwt.balanceOf(signers[2].address)).to.equal(ethers.parseEther("1"))
+        await(await gwt.approve(await exchange.getAddress(), ethers.parseEther("54600"))).wait();
+        const tx = await exchange.GWTtoDMC(ethers.parseEther("54600"));
 
-        // transfer from signers[2] success
-        await expect(gwt.connect(signers[2]).transfer(signers[1].address, ethers.parseEther("1")))
-            .emit(gwt, "Transfer").withArgs(signers[2].address, signers[1].address, ethers.parseEther("1"))
+        await expect(tx).to.changeTokenBalance(gwt, signers[0], ethers.parseEther("-54600"));
+        await expect(tx).to.changeTokenBalance(dmc, signers[0], ethers.parseEther("260"));
+        await expect(tx).to.changeTokenBalance(gwt, await exchange.getAddress(), ethers.parseEther("54600"));
 
-        expect(await gwt.balanceOf(signers[2].address)).to.equal(0)
-    })
-
-    it("burn", async () => {
-        let exchangeAddr = await exchange.getAddress();
-
-        await expect(gwt.connect(signers[1]).approve(exchangeAddr, ethers.parseEther("210")))
-            .emit(gwt, "Approval").withArgs(signers[1].address, exchangeAddr, ethers.parseEther("210"))
-
-        await expect(exchange.connect(signers[1]).exchangeDMC(ethers.parseEther("210")))
-            .emit(gwt, "Transfer").withArgs(signers[1].address, ethers.ZeroAddress, ethers.parseEther("210"))
-
-        expect(await gwt.balanceOf(signers[1].address)).to.equal(0)
-        expect(await dmc.balanceOf(signers[1].address)).to.equal(ethers.parseEther("1000"))
+        expect((await exchange.getCycleInfo())).to.deep.equal([4n, ethers.parseEther("0"), ethers.parseEther("260")])
     });
 
-    it("mint disallowed", async () => {
-        await expect(gwt.connect(signers[1]).mint(signers[1].address, ethers.parseEther("1"))).revertedWith("mint not allowed");
+    it("cycle 5", async () => {
+        // 前进到时间1000之后，开启下一轮
+        mine(2, {interval: 1000});
+        // 上一轮没有剩余，本轮的可兑换额度为210 + 100 / 2 = 260, 储存的可兑换额度为50
+        // 这次我们把260个都兑换掉, 需要260*210=54600个GWT
+        await(await gwt.approve(await exchange.getAddress(), ethers.parseEther("54600"))).wait();
+        const tx = await exchange.GWTtoDMC(ethers.parseEther("54600"));
+
+        await expect(tx).to.changeTokenBalance(gwt, signers[0], ethers.parseEther("-54600"));
+        await expect(tx).to.changeTokenBalance(dmc, signers[0], ethers.parseEther("260"));
+        await expect(tx).to.changeTokenBalance(gwt, await exchange.getAddress(), ethers.parseEther("54600"));
+
+        expect((await exchange.getCycleInfo())).to.deep.equal([5n, ethers.parseEther("0"), ethers.parseEther("260")])
     });
 })
