a1c3f6cdc07f0afa6be1a1e8a800bb69ba192cb8
weiqiushi <weiqiushi@buckyos.com>
2024-05-06 17:21:22 +0800

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'hardhat-abi-exporter';
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
 import '@openzeppelin/hardhat-upgrades';
+import "@nomicfoundation/hardhat-verify";
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -12,7 +13,8 @@ const config: HardhatUserConfig = {
                 enabled: true,
                 runs: 200,
             },
-            viaIR: true
+            viaIR: true,
+
         }
     },
     abiExporter: {
@@ -20,20 +22,40 @@ const config: HardhatUserConfig = {
         clear: true,
     },
     gasReporter: {
-        currency: 'USD',
-        gasPrice: 21,
-        enabled: true
+        enabled: false,
     },
     networks: {
-        x1test: {
-            url: "https://testrpc.x1.tech",
+        ethereum: {
+            url: "",
+            accounts: [
+                
+            ]
+        },
+        xtest: {
+            url: "https://testrpc.xlayer.tech",
             accounts: [
                 // add your private keys here
             ]
+        },
+        xlayer: {
+            url: "https://rpc.xlayer.tech"
+        },
+        hardhat: {
+            mining: {
+                auto: true,
+                interval: 15000,
+            }
+        },
+        localhost: {
+            gas: "auto"
         }
+    },
+    sourcify: {
+        enabled: true
     },
     mocha: {
         bail: true,
+        timeout: 24*60*60*1000
     }
 };
 
