bd69a86c91ee149d139dfda29af4f1ff16bc186d
liyaxing <liyaxing@buckyos.com>
2024-05-06 17:18:56 +0800

    Update node modules and congfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'hardhat-abi-exporter';
 import "hardhat-gas-reporter"
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
     solidity: {
